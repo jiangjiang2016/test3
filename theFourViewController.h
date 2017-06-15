f73dc1e5219e114edfecfafa3e3d131bcfcd97de
jiangjiang2016 <735660435@qq.com>
2017-06-15 21:53:42 +0800

    建立了
    建立了

---
+++ theFourViewController.h
@@ -0,0 +1,13 @@
+//
+//  theFourViewController.h
+//  test3
+//
+//  Created by 郭疆 on 2017/6/15.
+//  Copyright © 2017年 guojiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface theFourViewController : UIViewController
+
+@end
---
+++ theFourViewController.m
@@ -0,0 +1,37 @@
+//
+//  theFourViewController.m
+//  test3
+//
+//  Created by 郭疆 on 2017/6/15.
+//  Copyright © 2017年 guojiang. All rights reserved.
+//
+
+#import "theFourViewController.h"
+
+@interface theFourViewController ()
+
+@end
+
+@implementation theFourViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
