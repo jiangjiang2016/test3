c07092efb7f1928e5c8c5559a842e87b3e1a6171
jiangjiang2016 <735660435@qq.com>
2017-06-15 21:39:18 +0800

    Initial Commit

---
+++ test3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test3
+//
+//  Created by 郭疆 on 2017/6/15.
+//  Copyright © 2017年 guojiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test3/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test3
+//
+//  Created by 郭疆 on 2017/6/15.
+//  Copyright © 2017年 guojiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
