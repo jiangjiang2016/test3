7c8bbb0f32d80c37d728867874cd788b5a27e2b6
jiangjiang2016 <735660435@qq.com>
2017-06-15 21:43:49 +0800

    创建了第一个子页面哦
    创建了第一个子页面哦

--- test3/ViewController.m
+++ test3/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [[UILabel alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
