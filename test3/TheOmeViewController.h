61cc99742df052aca2d78cfa73dbb9df89ac2b28
jiangjiang2016 <735660435@qq.com>
2017-06-15 21:45:04 +0800

    新增一个子控制器

---
+++ test3/TheOmeViewController.h
@@ -0,0 +1,13 @@
+//
+//  TheOmeViewController.h
+//  test3
+//
+//  Created by 郭疆 on 2017/6/15.
+//  Copyright © 2017年 guojiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TheOmeViewController : UIViewController
+
+@end
---
+++ test3/TheOmeViewController.m
@@ -0,0 +1,37 @@
+//
+//  TheOmeViewController.m
+//  test3
+//
+//  Created by 郭疆 on 2017/6/15.
+//  Copyright © 2017年 guojiang. All rights reserved.
+//
+
+#import "TheOmeViewController.h"
+
+@interface TheOmeViewController ()
+
+@end
+
+@implementation TheOmeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
